2730aa0d277be19cf04e7437344d31e531635811
Clayton <persinger.clayton@gmail.com>
2023-06-14 14:59:17 -0700

    Add FavIcon component and implement state and onClick function

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,13 +1,20 @@
 import React, { useCallback, useState } from 'react';
 
-import { FavIcon } from './FavIcon';
+import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+
+  const [colour, setColour] = useState("")
+
+  const handleClick = () => {
+    setColour(colour ?  "" : "#C80000")
+  }
+
   return (
-    <div className="photo-list__fav-icon">
+    <div className="photo-list__fav-icon" onClick={handleClick}>
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <FavIcon  fill={colour}/>  
       </div>
     </div>
   );
