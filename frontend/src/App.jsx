31bb51668131cba8a4d46b075e4f175699332c0a
Clayton <persinger.clayton@gmail.com>
2023-06-14 15:03:43 -0700

    Add components and return statement

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 
 import PhotoListItem from './components/PhotoListItem';
+import PhotoFavButton from './components/PhotoFavButton';
+
 import './App.scss';
 
+
 // Note: Rendering a single component to build components in isolation
-const App = () => (
-  <div className="App">
-    <PhotoListItem/>
-  </div>
-)
+const App = () => {
+
+  const photos = [...Array(3)]
+  const list = photos.map((photo, i) => {
+    return <PhotoListItem key={i} />
+  })
+
+  return(
+    <div className="App">
+        {/* {list}  */}
+        
+        {/* <PhotoListItem />  */}
+        <PhotoFavButton />
+
+    </div>
+  )
+};
 
 export default App
