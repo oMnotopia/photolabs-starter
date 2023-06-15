ddbea8a843eb35548f3ed37ac0b62fbfc6044171
Clayton <persinger.clayton@gmail.com>
2023-06-14 18:39:30 -0700

    Remove old code for looping and remove default data

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -6,15 +6,8 @@ import PhotoListItem from './PhotoListItem';
 
 const PhotoList = (props) => {
 
-  const mappedPhotos = props.photos.map((photo) => {
-    return(
-      <img key={photo.id} src={photo.urls.regular}></img>
-    )
-  })
-
-  const photos = [...Array(3)]
-  const list = photos.map((photo, i) => {
-    return <PhotoListItem key={i} />
+  const list = props.photos.map((photo, i) => {
+    return <PhotoListItem key={i} urls={photo.urls}/>
   })
   
   return(
@@ -22,62 +15,6 @@ const PhotoList = (props) => {
       {list}
     </ul>
   )
-}
-
-PhotoList.defaultProps = {
-  photos: [
-    {
-      "id": "1",
-      "location": {
-        "city": "Montreal",
-        "country": "Canada"
-      },
-      "urls": {
-        "full": `${process.env.PUBLIC_URL}/Image-1-Full.jpeg`,
-        "regular": `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`
-      },
-      "user": {
-        "id": "1",
-        "username": "exampleuser",
-        "name": "Joe Example",
-        "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
-      }
-    },
-    {
-      "id": "2",
-      "location": {
-        "city": "Toronto",
-        "country": "Canada"
-      },
-      "urls": {
-        "full": `${process.env.PUBLIC_URL}/Image-2-Full.jpeg`,
-        "regular": `${process.env.PUBLIC_URL}/Image-2-Regular.jpeg`
-      },
-      "user": {
-        "id": "2",
-        "username": "exampleuser",
-        "name": "Joe Example",
-        "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
-      }
-    },
-    {
-      "id": "3",
-      "location": {
-        "city": "Ottawa",
-        "country": "Canada"
-      },
-      "urls": {
-        "full": `${process.env.PUBLIC_URL}/Image-3-Full.jpeg`,
-        "regular": `${process.env.PUBLIC_URL}/Image-3-Regular.jpeg`
-      },
-      "user": {
-        "id": "3",
-        "username": "exampleuser",
-        "name": "Joe Example",
-        "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
-      }
-    }
-   ]
 }
 
 export default PhotoList
