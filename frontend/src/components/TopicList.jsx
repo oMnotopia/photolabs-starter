94a8fd80abf0ccf5129a170690491a85c77c7b2a
Clayton <persinger.clayton@gmail.com>
2023-06-14 17:38:02 -0700

    Include ToplistItems and add HTMLand logic to display items

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
-import './TopicList.scss';
+import TopicListItem from './TopicListItem';
 
-const TopicList = () => {
-  <div className="top-nav-bar__topic-list">
-    {/* Insert React */}
-  </div>
+import '../styles/TopicList.scss';
+
+const TopicList = (props) => {
+  return(
+    <div className="top-nav-bar__topic-list">
+
+      {props.topics.map((topic, index )=> {
+        return <TopicListItem key={index} id={topic.id} slug={topic.slug} title={topic.title}/>
+      })}
+    </div>
+  )
 }
 
 TopicList.defaultProps = {
