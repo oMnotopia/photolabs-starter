895f994cfaddce9cf28483afa645aecc8c51ae16
Clayton <persinger.clayton@gmail.com>
2023-06-15 08:55:15 -0700

    Add logic for FavIcon to fill when a picture is liked

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -3,10 +3,14 @@ import FavIcon from './FavIcon';
 
 import '../styles/FavBadge.scss';
 
-export const FavBadge = ({ isFavPhotoExist }) => {
+export const FavBadge = ({ isFavPhotoExist, liked }) => {
+  let colour = ""
+
+  liked.length ? colour = "#C80000" : ""
+
   return (
     <div className='fav-badge'>
-      <FavIcon width={20} height={17} fill="#C80000" displayAlert={!!isFavPhotoExist}/>
+      <FavIcon width={20} height={17} fill={colour} displayAlert={!!isFavPhotoExist}/>
     </div>
   ) 
 };
