0fd504d51d5f868577386cbfa07c69fa697322b6
Clayton <persinger.clayton@gmail.com>
2023-06-14 18:40:41 -0700

    Remove default data

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -15,28 +15,4 @@ const TopicList = (props) => {
   )
 }
 
-TopicList.defaultProps = {
-  topics: [
-    {
-      "id": "1",
-      "slug": "topic-1",
-      "title": "Nature"
-    },  
-    {
-      "id": "2",
-      "slug": "topic-2",
-      "title": "Travel"
-    },
-    {
-      "id": "3",
-      "slug": "topic-3",
-      "title": "People"
-    },
-    {
-      "id": "4",
-      "slug": "topic-1",
-      "title": "Nature"
-    },  
-  ]
-}
 export default TopicList
