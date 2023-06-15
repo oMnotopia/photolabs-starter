9ee847f0dc520973a5fc19cc08d6160923c461a8
Clayton <persinger.clayton@gmail.com>
2023-06-14 17:36:17 -0700

    Update scss import to include .scss, and add return and HTML statements

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
-import '../styles/TopicListItem'
+import '../styles/TopicListItem.scss'
 
-const TopicListItem = () => {
-  <div className="topic-list__item">
-    {/* Insert React */}
-  </div>
+const TopicListItem = (props) => {
+  return (
+    <div className="topic-list__item">
+      <span>
+        {props.title}
+      </span>
+    </div>
+  )
 }
 
 TopicListItem.defaultProps =   {
