9d74f99cbb2abb0946a31346a568509e2b7ff9d0
Clayton <persinger.clayton@gmail.com>
2023-06-14 18:41:24 -0700

    Include mock data passed through props

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -6,11 +6,11 @@ import FavBadge from './FavBadge';
 import '../styles/TopNavigationBar.scss'
 
 
-const TopNavigation = () => {
+const TopNavigation = (props) => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList/> 
+      <TopicList topics={props.topics}/> 
       <FavBadge />
     </div>
   )
