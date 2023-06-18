94fac8849ad934a7f7f40533caafd032f859ff12
Clayton <persinger.clayton@gmail.com>
2023-06-18 15:02:45 -0700

    Replace fetch and state with axios and useApplicationData hook using Promise.all

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,62 +1,51 @@
 import React, { useState, useEffect } from 'react';
+import axios from "axios";
 
 import useApplicationData from './hooks/useApplicationData';
 
 import HomeRoute from './routes/HomeRoute';
 import PhotoDetailsModal from './routes/PhotoDetailsModal'
 
-// import photos from './mocks/photos';
-// import topics from './mocks/topics';
-
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
 
-  const {state, addToFavPhotoIds, removeFromFavPhotoIds, showModal, closeModal} = useApplicationData();
-
-  const [photos, setPhotos] = useState([]);
-  const [topics, setTopics] = useState([]);
+  const {state, addToFavPhotoIds, removeFromFavPhotoIds, showModal, closeModal, getData} = useApplicationData();
 
   useEffect(() => {
-
-    fetch('/api/photos')
-      .then(res => res.json())
-      .then(data => setPhotos(data))
-      .catch(err => console.log(err.message))
-    
-    fetch('/api/topics')  
-      .then(resp => resp.json())
-      .then(data => setTopics(data))
-      .catch(err => console.log(err.message))
-
-    
+    Promise.all([axios.get('/api/topics'), axios.get('/api/photos')]).then((res) => {
+      const [topics, photos] = res;
+      // console.log(topics.data, photos.data)
+      getData(topics.data, photos.data);
+    }).catch(err => console.log("An unexpected error occured", err))   
   }, [])
 
-  useEffect(() => {
-    const callThisFunc = ()  => {
-      fetch('/api/topics/photos/:topic_id')
-        .then(res => res.json())
-        .then(data => console.log(data))
+  // useEffect(() => {
+  //   const callThisFunc = ()  => {
+  //     console.log("In this effect")
+  //     fetch('/api/topics/photos/:topic_id')
+  //       .then(res => res.json())
+  //       .then(data => console.log(data))
 
-      const photoToBePassed = photoToBePassed = photos.filter(item => item.id === state.photos)[0]
-    }
+  //     // const photoToBePassed = photoToBePassed = photos.filter(item => item.id === state.photos)[0]
+  //   }
 
-  }, [state.isModalOpen])
+  //   if(state.isModalOpen === true) callThisFunc()
+
+  // }, [state.isModalOpen])
 
   return(
     <div className="App">
 
       <HomeRoute 
-        photos={photos} 
-        topics={topics} 
         state={state}
         addToFavPhotoIds={addToFavPhotoIds}
         removeFromFavPhotoIds={removeFromFavPhotoIds}
         showModal={showModal}
       />
       {state.isModalOpen && <PhotoDetailsModal 
-        photo={photoToBePassed}
+        photo={state.modalPhotoDetails[0]}
         state={state}
         closeModal={closeModal}
         addToFavPhotoIds={addToFavPhotoIds}
