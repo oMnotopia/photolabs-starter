acc84577aa67ad499014755ed6b0f8833b167072
Clayton <persinger.clayton@gmail.com>
2023-06-19 08:25:17 -0700

    Add click event to filter photos by topic

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -1,18 +1,35 @@
 import React from 'react';
+import axios from 'axios'
 
 import '../styles/TopicListItem.scss'
 
 const TopicListItem = (props) => {
+
+  const { id, title, getPhotosByTopic } = props
+
+  const handleClick = (id) => {
+    axios.get(`/api/topics/photos/${id}`)
+      .then((photos) => {
+        getPhotosByTopic(photos.data)
+      })
+      .catch((err) =>
+        console.log(
+          "An unexpected error occured attempting to perform request.",
+          err
+        )
+      );
+  }
+
   return (
-    <div className="topic-list__item">
+    <div className="topic-list__item" onClick={() => handleClick(id)}>
       <span>
-        {props.title}
+        {title}
       </span>
     </div>
   )
 }
 
-TopicListItem.defaultProps =   {
+TopicListItem.defaultProps = {
   "id": "1",
   "slug": "topic-1",
   "label": "Nature"
