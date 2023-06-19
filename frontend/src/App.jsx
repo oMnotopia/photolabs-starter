b59c7826763e22e777a29eeb5ad3549508d50960
Clayton <persinger.clayton@gmail.com>
2023-06-19 08:37:59 -0700

    Remove console.logs and include getPhotosByTopic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,29 +11,14 @@ import './App.scss';
 // Note: Rendering a single component to build components in isolation
 const App = () => {
 
-  const {state, addToFavPhotoIds, removeFromFavPhotoIds, showModal, closeModal, getData} = useApplicationData();
+  const {state, addToFavPhotoIds, removeFromFavPhotoIds, showModal, closeModal, getData, getPhotosByTopic} = useApplicationData();
 
   useEffect(() => {
     Promise.all([axios.get('/api/topics'), axios.get('/api/photos')]).then((res) => {
       const [topics, photos] = res;
-      // console.log(topics.data, photos.data)
       getData(topics.data, photos.data);
     }).catch(err => console.log("An unexpected error occured", err))   
   }, [])
-
-  // useEffect(() => {
-  //   const callThisFunc = ()  => {
-  //     console.log("In this effect")
-  //     fetch('/api/topics/photos/:topic_id')
-  //       .then(res => res.json())
-  //       .then(data => console.log(data))
-
-  //     // const photoToBePassed = photoToBePassed = photos.filter(item => item.id === state.photos)[0]
-  //   }
-
-  //   if(state.isModalOpen === true) callThisFunc()
-
-  // }, [state.isModalOpen])
 
   return(
     <div className="App">
@@ -43,6 +28,7 @@ const App = () => {
         addToFavPhotoIds={addToFavPhotoIds}
         removeFromFavPhotoIds={removeFromFavPhotoIds}
         showModal={showModal}
+        getPhotosByTopic={getPhotosByTopic}
       />
       {state.isModalOpen && <PhotoDetailsModal 
         photo={state.modalPhotoDetails[0]}
