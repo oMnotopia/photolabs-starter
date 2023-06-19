549683978ad412311a44decdc6a76dc8625e50e7
Clayton <persinger.clayton@gmail.com>
2023-06-19 12:29:50 -0700

    Include favPhoto

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -5,7 +5,7 @@ import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton(props) {
 
-  const { id, liked, addToFavPhotoIds, removeFromFavPhotoIds } = props
+  const { id, liked, favPhoto, addToFavPhotoIds, removeFromFavPhotoIds } = props
 
   const handleClick = () => {
     if (liked.includes(id)) {
@@ -18,7 +18,7 @@ function PhotoFavButton(props) {
   return (
     <div className="photo-list__fav-icon" onClick={handleClick}>
       <div className="photo-list__fav-icon-svg">
-        <FavIcon fill={liked.includes(id) ? "#C80000" : ""}/>  
+        <FavIcon fill={favPhoto ? "#C80000" : ""}/>  
       </div>
     </div>
   );
