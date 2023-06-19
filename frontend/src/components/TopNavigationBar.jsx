ddc320e5c565af825706694881112fa9245ba873
Clayton <persinger.clayton@gmail.com>
2023-06-19 08:36:58 -0700

    Include getPhotosByTopic

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -6,12 +6,15 @@ import FavBadge from './FavBadge';
 import '../styles/TopNavigationBar.scss'
 
 
-const TopNavigation = ( { state } ) => {
+const TopNavigation = (props) => {
+
+  const { state, getPhotosByTopic } = props
 
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
       <TopicList 
+        getPhotosByTopic={getPhotosByTopic}
         topics={state.topics}
       /> 
       <FavBadge 
